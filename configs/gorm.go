8a9e193532fbace23841c0a85eb47bbd61749c71
Gustavo <gustascrows@gmail.com>
2023-05-12 20:48:58 -0300

    Corrigindo arquivos

--- configs/gorm.go
+++ configs/gorm.go
@@ -21,7 +21,7 @@ func ConnectGorm() {
 		panic(err)
 	}
 	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUser, config.DBPassword, config.DBPort, config.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
--- internal/infra/database/user_db_test.go
+++ internal/infra/database/user_db_test.go
@@ -1,28 +1,22 @@
 package database
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/Scrowszinho/api-go-products/configs"
 	"github.com/Scrowszinho/api-go-products/internal/entity"
+	"github.com/Scrowszinho/api-go-products/migrations"
 	"github.com/stretchr/testify/assert"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
 )
 
 func TestCreateUser(t *testing.T) {
-	config, err := configs.GetConfigs(".")
-	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUser, config.DBPassword, config.DBPort, config.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		t.Error(err)
-	}
-	db.AutoMigrate(&entity.User{})
-	user, _ := entity.NewUser("Gustavo", "Test", "gustavo@gmail.com", "123456", "Test", 0.0)
+	configs.ConnectGorm()
+	db := configs.GetDB()
+	migrations.MigrateTable()
+	user, _ := entity.NewUser("Gustavo", "Test", "gustavo@gmail.com", "123456", "Test", 1000.0)
 	userDB := NewUser(db)
 
-	err = userDB.Create(user)
+	err := userDB.Create(user)
 	assert.Nil(t, err)
 
 	var userFounder entity.User
@@ -35,20 +29,13 @@ func TestCreateUser(t *testing.T) {
 }
 
 func TestByEmail(t *testing.T) {
-	config, err := configs.GetConfigs(".")
-	if err != nil {
-		panic(err)
-	}
-	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.DBUser, config.DBPassword, config.DBPort, config.DBName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		t.Error(err)
-	}
-	db.AutoMigrate(&entity.User{})
+	configs.ConnectGorm()
+	db := configs.GetDB()
+	migrations.MigrateTable()
 	user, _ := entity.NewUser("Gustavo", "Test", "gustavo@gmail.com", "123456", "Test", 1000.0)
 	userDB := NewUser(db)
 
-	err = userDB.Create(user)
+	err := userDB.Create(user)
 	assert.Nil(t, err)
 
 	userFound, err := userDB.FindByEmail(user.Email)
